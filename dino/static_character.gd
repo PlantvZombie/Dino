740f110a031d1c137a4d0225edda866350863a6a
NoahGBaker <palifonix@gmail.com>
2025-04-11 23:53:23 -0400

    c

--- dino/static_character.gd
+++ dino/static_character.gd
@@ -1,19 +1,22 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
+		velocity.y = move_toward(velocity.y, 200, 5)
+	if is_on_floor():
+		velocity.y = 0
 	if Input.is_action_just_pressed("up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	
+		velocity.y -= 150
+
+	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_released("up"):
+		if velocity.y < 0:
+			velocity.y *= 0.3
 	if Input.is_action_pressed("down") and is_on_floor():
 		# anim.play(down)
 		$Down.set_deferred("disabled", false)
@@ -24,5 +27,3 @@ func _physics_process(delta: float) -> void:
 		$Down.set_deferred("disabled", true)
 		$Standing.set_deferred("disabled", false)
 		print("down-release")
-
-	move_and_slide()
