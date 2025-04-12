0722bc788b6f04a79ac9d71765376edc6ac5732a
PlantvZombie <wintonajones@gmail.com>
2025-04-12 10:21:45 -0400

    Added Global Scene + Some Dino Anims

--- dino/dino_story_mode.gd
+++ dino/dino_story_mode.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y = move_toward(velocity.y, 200, 5)
 	if is_on_floor():
 		velocity.y = 0
+		$"Dino Sprite".play("run")
 	if Input.is_action_just_pressed("up") and is_on_floor():
+		$"Dino Sprite".play("jump")
 		velocity.y -= 150
 	velocity.x = 200
 	move_and_slide()
