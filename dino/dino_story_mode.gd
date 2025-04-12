73fbfbd40064f7332bc5861e65f2deb43f4c9b11
PlantvZombie <wintonajones@gmail.com>
2025-04-12 10:09:45 -0400

    Fixed Story Mode

--- dino/dino_story_mode.gd
+++ dino/dino_story_mode.gd
@@ -8,11 +8,17 @@ func _physics_process(delta: float) -> void:
 		velocity.y = move_toward(velocity.y, 200, 5)
 	if is_on_floor():
 		velocity.y = 0
-	if Input.is_action_just_pressed("space") and is_on_floor():
-		velocity.y -= 200
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		velocity.y -= 150
+	velocity.x = 200
 	move_and_slide()
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_released("space"):
+	if event.is_action_released("up"):
 		if velocity.y < 0:
-			velocity.y *= 0.2
+			velocity.y *= 0.3
+
+
+func _on_hitbox_detection_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Obstacles"):
+		get_tree().reload_current_scene()
