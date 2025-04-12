2539f1b84742fdc9a0863da53a249bae9dc9fa39
NoahGBaker <palifonix@gmail.com>
2025-04-12 10:55:03 -0500

    ghnikjerstl

--- dino/static_character.gd
+++ dino/static_character.gd
@@ -5,18 +5,18 @@ extends CharacterBody2D
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y = move_toward(velocity.y, 200, 5)
+		velocity.y = move_toward(velocity.y, 250, 3)
 	if is_on_floor():
 		velocity.y = 0
 	if Input.is_action_just_pressed("up") and is_on_floor():
-		velocity.y -= 175
+		velocity.y -= 145
 
 	move_and_slide()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_released("up"):
 		if velocity.y < 0:
-			velocity.y *= 0.3
+			velocity.y *= 0.1
 	if Input.is_action_just_pressed("down"):
 		# anim.play(down)
 		velocity.y += 225
