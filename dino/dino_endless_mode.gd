0c34a90f4e772d43f7d105c062105050cf6b35a7
NoahGBaker <palifonix@gmail.com>
2025-04-12 20:43:22 -0500

    Revert "fixers"
    This reverts commit e5cf6118f815f15774f5b0e9e9f39db00e643cd9.

--- dino/dino_endless_mode.gd
+++ dino/dino_endless_mode.gd
@@ -10,15 +10,12 @@ const SPEED = 300
 @export var wRock:PackedScene
 @export var waRock:PackedScene
 @export_category("Area Two")
-@export var numObjArea2:int = 3
+@export var numObjArea2:int = 2
 @export var stalacT:PackedScene
 @export var stalacM:PackedScene
-@export var stalacD:PackedScene
 @export_category("Platforms")
 @export var platform:PackedScene
 @export var cavePlatform:PackedScene
-@export var caveE:PackedScene
-@export var caveE2:PackedScene
 var currArea = 1
 var rock
 var plat
@@ -45,7 +42,7 @@ func _process(delta: float) -> void:
 		timer -= 4.783
 		if currArea == 1:
 			plat = platform.instantiate()
-		if currArea == 0:
+		if currArea == 2:
 			plat = cavePlatform.instantiate()
 		plat.position = endLoc.position
 		add_child(plat)
@@ -67,12 +64,10 @@ func chooseRock(area:int):
 		if (item == 4):
 			rock = waRock.instantiate()
 	else:
-		item = randi_range(1, numObjArea2)
+		item = randi_range(1, 2)
 		if (item == 1):
 			rock = stalacT.instantiate()
 		if (item == 2):
-			rock = stalacM.instantiate()
-		if (item == 3):
 			rock = stalacM.instantiate()
 	rock.position = spawnLoc.position
 	add_child(rock)
@@ -82,10 +77,7 @@ func chooseRock(area:int):
 func _on_rock_timer_timeout() -> void:
 	randomize()
 	chooseRock(currArea)
-	if currArea == 1:
-		$RockTimer.wait_time = randf_range(0.2, 0.4)
-	if currArea == 1:
-		$RockTimer.wait_time = randf_range(0.7, 1.5)
+	$RockTimer.wait_time = randf_range(0.7, 1.5)
 
 
 func _on_timer_timeout() -> void:
@@ -99,14 +91,6 @@ func _on_timer_timeout() -> void:
 	if score % 500 == 0:
 		speed_factor -= 2.5
 		level += 1
-		currArea = level % 2
-		var cave = caveE.instantiate()
-		cave.position = spawnLoc.position
-		add_child(cave)
-		$RockTimer.wait_time += 1
-		if currArea == 2:
-			$/root/Global.isInCave = true
-		else:
-			$/root/Global.isInCave = false
+		currArea = level % 3
 	$CanvasLayer/Label.text = "Score: %s" % score
 	$CanvasLayer/Level.text = "\nLevel: %s" % level
