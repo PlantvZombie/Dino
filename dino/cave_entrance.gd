4a2357adf83afb7d104bb6bb463484d1c1c6d381
NoahGBaker <palifonix@gmail.com>
2025-04-12 22:27:06 -0400

    Reapply "Merge branch 'main' of https://github.com/PlantvZombie/Dino"
    This reverts commit 02124ca1a95eb9cd4f30f3aec1784e99740253e2.

---
+++ dino/cave_entrance.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+
+func _physics_process(delta: float) -> void:
+	delta *= $/root/Global.speed_factor
+	position += transform.x * ($/root/Global.speed / 2) * delta
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	delta *= $/root/Global.speed_factor
+	var viewport_size = get_viewport().get_size()
+	
+	# Check if the node is outside the screen
+	if position.x < -1000 or position.x > viewport_size.x or position.y < -1000 or position.y > viewport_size.y:
+		queue_free()
